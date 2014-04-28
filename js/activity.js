aab27c3d6cafc3d6847b3836604ecdabe77039c7
Puneet Kaur <puneet.gkaur@gmail.com>
2014-04-28 18:50:59 +0530

    initial commit

---
+++ js/activity.js
@@ -0,0 +1,16 @@
+var activity_cordova;
+
+define(function (require) {
+    var activity = require("sugar-web/activity/activity");
+
+    // Manipulate the DOM only when it is ready.
+    require(['domReady!'], function (doc) {
+ 
+        // Initialize the activity.
+        activity.setup();
+	activity_cordova = activity.cordova;
+
+
+    });
+
+});
